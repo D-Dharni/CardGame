f29969811e9f1bad846e1287ecd36ede812068af
devendharni <deven.dharni@gmail.com>
2024-11-17 11:57:29 -0800

    Edited the Player class:
    - Added Getters
    - The adding methods
    - Constructors
    - To String
    
    Created the other functions for later

--- src/Card.java
+++ src/Card.java
@@ -1,4 +1,47 @@
-package PACKAGE_NAME;
-
 public class Card {
+    // Instance Variables
+    private String rank;
+    private String suit;
+    private int value;
+
+    // Constructor
+    public Card(String rank, String suit, int value) {
+        this.rank = rank;
+        this.suit = suit;
+        this.value = value;
+    }
+
+    // Getter Methods
+    public String getRank() {
+        return rank;
+    }
+
+    public String getSuit() {
+        return suit;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+
+    // Setter Methods
+    public void setRank(String rank) {
+        this.rank = rank;
+    }
+
+    public void setSuit(String suit) {
+        this.suit = suit;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+
+    // To String Function
+    public String toString()
+    {
+        return this.rank + " of " + this.suit;
+    }
 }
---
+++ src/Player.java
@@ -0,0 +1,50 @@
+import java.util.ArrayList;
+
+public class Player {
+    // Instance Variables
+    private String name;
+    private ArrayList<Card> hand;
+    private int points;
+
+
+    // Constructors
+    public Player(String name) {
+        this.name = name;
+        this.points = 0;
+    }
+
+    public Player(String name, ArrayList<Card> hand) {
+        this.name = name;
+        this.hand = hand;
+        this.points = 0;
+    }
+
+
+    // Getter Methods
+    public String getName() {
+        return name;
+    }
+
+    public ArrayList<Card> getHand() {
+        return hand;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    // Adding Points
+    public void addPoints(int points) {
+        this.points += points;
+    }
+
+    public void addCard(Card card) {
+        this.hand.add(card);
+    }
+
+    // toString Method
+    public String toString() {
+        return this.name + " has " + this.points + "\n" + this.name + "'s cards: " + this.hand;
+    }
+
+}
